8e9e3755d70f4683ded6250eaa08eb5ec13652e9
dtis96 <drewt96@gmail.com>
2017-12-15 15:45:04 -0500

    Added a functional "health bar"

---
+++ Assets/Scripts/WalkingBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingBar : MonoBehaviour {
+
+    public float TotalTime;
+    public float TimeLeft;
+
+	void Start () {
+        TimeLeft = TotalTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            LowerTime();
+        }
+	}
+
+    void LowerTime()
+    {
+        if(TimeLeft > 0)
+        {
+            TimeLeft -= 1;
+            transform.localScale = new Vector3((TimeLeft / TotalTime), 1, 1);
+        }
+    }
+}
