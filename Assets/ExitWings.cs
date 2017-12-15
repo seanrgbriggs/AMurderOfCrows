8fba48f32167baa29be332ba62238339c0027d59
seanrgbriggs <seanrgbriggs@gmail.com>
2017-12-15 16:54:32 -0500

    wings over us

---
+++ Assets/ExitWings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitWings : MonoBehaviour {
+
+    public int radius;
+    List<Player> allPlayers
+    {
+        get
+        {
+            return GameController.instance.players;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        System.Predicate<Player> nearTheWings = player => Vector2.Distance(transform.position, player.transform.position) <= radius;
+        int numPresent = allPlayers.FindAll(player => nearTheWings(player) && !player.isMurderer).Count;
+
+        if(numPresent >= allPlayers.Count - 1)
+        {
+            int numKeys = 0;
+            allPlayers.ForEach(player => numKeys += player.keysCollected);
+            if(numKeys == 4)
+            {
+                print("win!");
+                Destroy(gameObject);
+            }
+        }
+        		
+	}
+}
